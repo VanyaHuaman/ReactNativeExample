6fc827b6189738ec7cb487267e4ad2f7a3ac16e4
Vanya Huaman <VHuaman@caesars.com>
2024-01-24 20:08:40 -0600

    add scripts to package json

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,13 @@
+import com.facebook.react.utils.KotlinStdlibCompatUtils.toBooleanStrictOrNullCompat
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.facebook.react")
 }
+
+apply(from = "../reactConfig.gradle")
+
+val jscFlavor = "org.webkit:android-jsc-intl:+"
 
 android {
     namespace = "com.example.reactnativeexample"
@@ -55,10 +60,16 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("com.facebook.react:react-android")
-    implementation("com.facebook.react:hermes-android")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val hermesEnabled: String by rootProject
+    if (hermesEnabled.toBooleanStrictOrNullCompat() == true) {
+        implementation("com.facebook.react:hermes-android")
+    } else {
+        implementation(jscFlavor)
+    }
 }
 
 apply(from = "../../node_modules/@react-native-community/cli-platform-android/native_modules.gradle")
